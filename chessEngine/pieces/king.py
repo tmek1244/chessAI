eed01e1d853b0d4297912041bfc4e3035a5c277d
tmek1244 <tmek1244@wp.pl>
2022-08-02 22:28:08 +0200

    Check if king is under the check

--- chessEngine/pieces/king.py
+++ chessEngine/pieces/king.py
@@ -12,8 +12,8 @@ class King(Piece):
         row_dest, col_dest = translate(end)
 
         # normal move
-        if abs(row_start - row_dest) + abs(col_start - col_dest) == 1:
-            return not self.under_check()
+        if abs(row_start - row_dest) <= 1 and abs(col_start - col_dest) <= 1:
+            return not self.under_check(board, (row_dest, col_dest))
 
         # Board makes sure that both rook and king haven't moved yet
         # short castle
@@ -24,16 +24,59 @@ class King(Piece):
 
         return False
 
-    def under_check(self, position: Coords = None):
+    def _run_trace(self, cur_position, ver, hor, board):
+        counter = 1
+        
+        for position in [(cur_position[0] + ver * i, cur_position[1] + hor * i) for i in range(1, 8)]:
+            if position[0] > 7 or position[0] < 0 or position[1] > 7 or position[1] < 0:
+                return None
+            if board[position]:
+                return None if board[position].color == self.color else (board[position], counter)
+            counter += 1
+        return None
+            
+
+    def under_check(self, board, position: Coords = None):
+        for direction in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            piece_n_counter = self._run_trace(position, *direction, board)
+            if piece_n_counter:
+                piece, counter = piece_n_counter
+                if piece.type == PieceType.QUEEN or piece.type == PieceType.ROOK:
+                    return True
+                if counter == 1 and piece.type == PieceType.KING:
+                    return True
+        
+        for direction in [(1, 1), (1, -1)]:
+            piece_n_counter = self._run_trace(position, *direction, board) 
+            if piece_n_counter:
+                piece, counter = piece_n_counter
+                if piece.type == PieceType.QUEEN or piece.type == PieceType.BISHOP:
+                    return True
+                if counter == 1 and piece.type == PieceType.KING:
+                    return True
+                if self.color == PieceColor.WHITE and counter == 1 and piece.type == PieceType.PAWN:
+                    return True
+        
+        for direction in [(-1, 1), (-1, -1)]:
+            piece_n_counter = self._run_trace(position, *direction, board) 
+            if piece_n_counter:
+                piece, counter = piece_n_counter
+                if piece.type == PieceType.QUEEN or piece.type == PieceType.BISHOP:
+                    return True
+                if counter == 1 and piece.type == PieceType.KING:
+                    return True
+                if self.color == PieceColor.BLACK and counter == 1 and piece.type == PieceType.PAWN:
+                    return True
+
         return False
 
     def _can_castle_short(self, row_dest, col_dest, board: BoardField):
         return (
             (not board.is_between(self.position, (row_dest, 7))) and
             (not self.when_moved) and
-            (not self.under_check(self.position)) and
-            (not self.under_check((row_dest, 5))) and
-            (not self.under_check((row_dest, 6))) and
+            (not self.under_check(board, self.position)) and
+            (not self.under_check(board, (row_dest, 5))) and
+            (not self.under_check(board, (row_dest, 6))) and
             (row_dest == 0 if self.color == PieceColor.WHITE else row_dest == 7) and
             (col_dest == 6) and
             (board[(row_dest, 7)].type == PieceType.ROOK) and
@@ -44,9 +87,9 @@ class King(Piece):
         return (
             (not self.when_moved) and
             (not board.is_between(self.position, (row_dest, 0))) and
-            (not self.under_check(self.position)) and
-            (not self.under_check((row_dest, 3))) and
-            (not self.under_check((row_dest, 2))) and
+            (not self.under_check(board, self.position)) and
+            (not self.under_check(board, (row_dest, 3))) and
+            (not self.under_check(board, (row_dest, 2))) and
             (row_dest == 0 if self.color == PieceColor.WHITE else row_dest == 7) and
             (col_dest == 2) and
             (board[(row_dest, 0)].type == PieceType.ROOK) and
