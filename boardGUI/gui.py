8bb4c43355cf31dae224cdef679d17aa00e0e81a
tmek1244 <tmek1244@wp.pl>
2022-09-16 00:45:44 +0200

    Add box with promotion pieces

--- boardGUI/gui.py
+++ boardGUI/gui.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 
 
 from chessEngine.chessEngine import Board
-from chessEngine.common import PieceType
+from chessEngine.common import PieceColor, PieceType
 
 from .piece import PieceGUI
 
@@ -26,6 +26,8 @@ class BoardGui:
         self.pieces_gui: PieceGUI = pieces_gui
         self.piece_pressed = None
         self.possible_moves: list[tuple[int, int]] = []
+        self.choosing_promotion: bool = False
+        self.promotion_square: tuple[int, int]
 
     def run(self):
         while self.running:
@@ -49,6 +51,25 @@ class BoardGui:
                 move_indicator, 
                 (SCREEN_HEIGHT//8*(move[1]), SCREEN_WIDTH//8*(7 - move[0]))
             )
+    
+    def draw_promotion_box(self):
+        pygame.draw.rect(self.screen, (220,220,220), pygame.Rect(SCREEN_WIDTH//2-200, SCREEN_HEIGHT//2-50, 400, 100))
+        pygame.draw.rect(self.screen, (0,0,0), pygame.Rect(SCREEN_WIDTH//2-200, SCREEN_HEIGHT//2-50, 400, 100), 5)
+
+        color = self.engine_board.next_move
+
+        self.screen.blit(self.pieces_gui.get_image(PieceType.QUEEN, color), (
+                SCREEN_WIDTH//2-200, SCREEN_HEIGHT//2-50
+                ))
+        self.screen.blit(self.pieces_gui.get_image(PieceType.ROOK, color), (
+                SCREEN_WIDTH//2-200+100, SCREEN_HEIGHT//2-50
+                ))
+        self.screen.blit(self.pieces_gui.get_image(PieceType.BISHOP, color), (
+                SCREEN_WIDTH//2-200+200, SCREEN_HEIGHT//2-50
+                ))
+        self.screen.blit(self.pieces_gui.get_image(PieceType.KNIGHT, color), (
+                SCREEN_WIDTH//2-200+300, SCREEN_HEIGHT//2-50
+                ))
         
     def loop(self):
         self.set_board()
@@ -64,13 +85,22 @@ class BoardGui:
                 ))
         
         ev = pygame.event.get()
-        self.engine_board.bot_move()
+        # self.engine_board.bot_move()
         # sleep(1)
         # proceed events
         for event in ev:
-
-            # handle MOUSEBUTTONUP
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.choosing_promotion:
+                if event.type != pygame.MOUSEBUTTONDOWN:
+                    continue
+                pos = pygame.mouse.get_pos()
+                if SCREEN_WIDTH//2-200 < pos[0] < SCREEN_WIDTH//2+200 and SCREEN_HEIGHT//2-50 < pos[1] < SCREEN_HEIGHT//2+50:
+                    figure_id = 7 - pos[0]//(SCREEN_WIDTH//8)
+                    print(f"IN, CHOOSEN {PieceType(figure_id)}")
+                    col, row = self.promotion_square[0]//(SCREEN_WIDTH//8), (7 - self.promotion_square[1]//(SCREEN_HEIGHT//8))
+                    self.engine_board.make_move(self.piece_pressed.position, (row, col), PieceType(figure_id))
+                    self.choosing_promotion = False
+                    self.piece_pressed = None
+            elif event.type == pygame.MOUSEBUTTONDOWN and not self.choosing_promotion:
                 pos = pygame.mouse.get_pos()
                 log.debug(pos)
                 col, row = pos[0]//(SCREEN_WIDTH//8), (7 - pos[1]//(SCREEN_HEIGHT//8))
@@ -90,13 +120,16 @@ class BoardGui:
 
                     log.debug(f"{col}, {row}")
                     if self.engine_board.make_move(self.piece_pressed.position, (row, col)) == 2: # promotion
-                        self.engine_board.make_move(self.piece_pressed.position, (row, col), PieceType.QUEEN)
-
+                        self.choosing_promotion = True
+                        self.promotion_square = pos
                     # self.piece_pressed.x = col
                     # self.piece_pressed.y = row
-                    self.piece_pressed = None
+                    else:
+                        self.piece_pressed = None
                     self.possible_moves = []
-                
+        if self.choosing_promotion:
+            self.draw_promotion_box()
+            # pygame.draw.rect(self.screen, (255,0,0), pygame.Rect(30, 30, 60, 60))
             
         pygame.display.flip()
             
